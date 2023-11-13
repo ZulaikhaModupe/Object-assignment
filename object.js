f6ab01a95fd1a6994063e3ce11f7260fa73756db
ZulaikhaModupe <zulaikhabuari@gmail.com>
2023-11-13 20:27:37 +0100

    first commit

---
+++ object.js
@@ -0,0 +1,25 @@
+//1
+const folder = {
+    // name: 'John',
+    // name: 'Pete',
+    surname: 'Smith',
+    
+};
+
+
+let salaries = {
+    Chidi: 100,
+    Justice: 160,
+    Zulaikha: 130,
+  }
+
+
+  function sumSalaries(a, b, c) {
+    return a + b + c;
+  }
+
+  let result = sumSalaries(salaries.Chidi, salaries.Justice, salaries.Zulaikha);
+  console.log(result);
+
+
+  
